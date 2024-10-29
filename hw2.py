39b670f16230c66540b592df578718b849692688
Aaliyah <akelly47@calpoly.edu>
2024-10-29 13:40:07 -0700

    push 1

--- hw2.py
+++ hw2.py
@@ -1,12 +1,38 @@
 import data
+from data import Point, Rectangle, Duration
+
 
 # Write your functions for each part in the space below.
 
 # Part 1
+# Create a Rectangle object from two Point objects by determining
+# the top left/ bottom right corners based on x and y values
+# input: two Point objects
+# output: a Rectangle object
+def create_rectangle(point1: Point, point2: Point) -> Rectangle:
+    #determine top left/ bottom right Point object
+    top_left_x = min(point1.x, point2.x)
+    bottom_right_x = max(point1.x, point2.x)
+    top_left_y = max(point1.y, point2.y)
+    bottom_right_y = min(point1.y, point2.y)
 
+    #top left/ bottom right Point objects
+    top_left = Point(top_left_x, top_left_y)
+    bottom_right = Point(bottom_right_x, bottom_right_y)
+
+    return Rectangle(top_left, bottom_right)
 
 # Part 2
+# Define a function to check if one duration is shorter than another.
+# input: two Duration objects
+# output: bool showing if first Duration is shorter than the second
+def shorter_duration_than(dur1: Duration, dur2: Duration) -> bool:
+    # Convert both durations to total seconds for comparison
+    seconds1 = dur1.minutes * 60 + dur2.seconds
+    seconds2 = dur2.minutes * 60 + dur2.seconds
 
+    # Return True if dur1 is shorter than dur2, otherwise False
+    return seconds1 < seconds2
 
 # Part 3
 
--- hw2_tests.py
+++ hw2_tests.py
@@ -2,15 +2,40 @@ import data
 import hw2
 import unittest
 
+from data import Duration
+from hw2 import Point, create_rectangle, shorter_duration_than
+
 
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_create_rectangle_standard(self):
+        point1 = Point(2, 2)
+        point2 = Point(10, 10)
+        rectangle = create_rectangle(point1, point2)
 
+        self.assertEqual(rectangle.top_left, Point(2, 10))
+        self.assertEqual(rectangle.bottom_right, Point(10, 2))
+
+    def test_create_rectangle_swapped(self):
+        point1 = Point(10, 10)
+        point2 = Point(2, 2)
+        rectangle = create_rectangle(point1, point2)
+
+        self.assertEqual(rectangle.top_left, Point(2, 10))
+        self.assertEqual(rectangle.bottom_right, Point(10, 2))
 
     # Part 2
+    def test_dur(self):
+        dur1 = Duration (4, 30)
+        dur2 = Duration(5, 0)
+        self.assertTrue(shorter_duration_than(dur1, dur2))
 
+    def test_shorter_duration_than_false(self):
+        duration1 = Duration(6, 0)
+        duration2 = Duration(5, 59)
+        self.assertFalse(shorter_duration_than(duration1, duration2))
 
     # Part 3
 
